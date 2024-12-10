37576ac43175b5dddeae2da6d0f464dbe6a52337
Edwin Chang <edwinchang2024@gmail.com>
2024-12-09 23:17:37 -0800

    day10

---
+++ src/day10/10.part2.kt
@@ -0,0 +1,16 @@
+package day10
+
+import java.io.File
+
+fun main() {
+    val grid = File("i").readLines()
+    println(grid.flatMapIndexed { index, row -> row.indices.filter { row[it] == '0' }.map { index to it } }
+        .sumOf { coords ->
+            fun find(c: Pair<Int, Int>): List<Pair<Int, Int>> = listOf(
+                c.first - 1 to c.second, c.first + 1 to c.second, c.first to c.second - 1, c.first to c.second + 1
+            ).filter {
+                it.first in grid.indices && it.second in grid[0].indices && grid[it.first][it.second].digitToInt() == grid[c.first][c.second].digitToInt() + 1
+            }.flatMap { if (grid[it.first][it.second] == '9') listOf(it) else find(it) }
+            find(coords).size
+        })
+}
